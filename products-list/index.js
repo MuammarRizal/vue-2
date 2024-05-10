5243720b25b053d34fd7644d772e80a2e9a8f0c7
MuammarRizal <muammarrizal31@gmail.com>
2024-05-10 16:30:01 +0700

    add files

--- products-list/index.js
+++ products-list/index.js
@@ -1,3 +1,86 @@
+Vue.component("price", {
+  props: {
+    price: {
+      type: String,
+    },
+    prefix: {
+      type: String,
+      default: "$",
+    },
+    precision: {
+      type: Number,
+      default: 2,
+    },
+  },
+  template: `
+  <span>
+    {{this.prefix}}{{Number.parseFloat(this.price).toFixed(this.precision)}}
+  </span>`,
+});
+
+Vue.component("product-list", {
+  props: ["products", "maximum"],
+  methods: {
+    before: function (el) {
+      el.className = "d-flex m-1";
+    },
+    enter: function (el) {
+      const delay = el.dataset.index * 100;
+      setTimeout(() => {
+        el.className = "d-flex m-1 fade-enter-active";
+      }, delay);
+    },
+    leave: function (el) {
+      const delay = el.dataset.index * 100;
+      setTimeout(() => {
+        el.className = "d-none m-1 animate__animated animate__fadeOutDown";
+      }, delay);
+    },
+  },
+  template: `
+  <transition-group class="container d-flex justify-content-center flex-wrap" name="custom" tag="div"
+  @before-enter="before"
+  @enter="enter"
+  @leave="leave"
+>
+  >
+  <div
+    class="m-1 d-none"
+    v-for="(item, index) in products"
+    v-if="item.price <= Number(maximum)"
+    :key="item.id"
+    :data-index="index"
+  >
+    <div class="card" style="width: 18rem" v>
+      <!-- tanpa menggunakan v-bind pun bisa menggunakan : seperti ini -->
+      <img
+        :src="item.image"
+        v-bind:alt="item.image_title"
+        class="card-img-top"
+      />
+      <div class="card-body">
+        <h5 class="card-title">{{item.name}}</h5>
+        <p class="card-text">{{item.description}}</p>
+        <p class="card-text">
+          <price
+            :price="item.price"
+            :precision="2"
+            v-bind:prefix="'$'"
+          ></price>
+        </p>
+        <div class="d-flex justify-content-between">
+          <a :href="item.image" class="btn btn-primary">Show Image</a>
+          <button class="btn btn-info" @click="$emit('add-to-cart',item)">
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</transition-group>
+  `,
+});
+
 const app = new Vue({
   el: "#app",
   data: {
@@ -61,22 +144,6 @@ const app = new Vue({
     },
     toggleClick: function () {
       this.style.displayState = !this.style.displayState;
-    },
-
-    before: function (el) {
-      el.className = "d-flex m-1";
-    },
-    enter: function (el) {
-      const delay = el.dataset.index * 100;
-      setTimeout(() => {
-        el.className = "d-flex m-1 fade-enter-active";
-      }, delay);
-    },
-    leave: function (el) {
-      const delay = el.dataset.index * 100;
-      setTimeout(() => {
-        el.className = "d-none m-1 animate__animated animate__fadeOutDown";
-      }, delay);
     },
 
     removeItem: function (index) {
